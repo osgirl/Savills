331fdd0b7cd393131a0aa9dc5a60e8f566cf80e9
Nhieu hoang <nhjuhoang@gmail.com>
2018-11-03 09:57:50 +0700

    ..

--- src/screens/faq/layout.js
+++ src/screens/faq/layout.js
@@ -2,29 +2,24 @@ import React, { Component } from 'react';
 import {
     View,
     Dimensions,
-    ScrollView
+    ScrollView,
+    WebView
 } from 'react-native';
-
-import HTML from 'react-native-render-html';
 
 export default class extends Component {
 
     render() {
         return (
-            <ScrollView
-                style={{ flex: 1, backgroundColor: '#F6F8FD' }}>
+            <View style={{ flex: 1, }}>
                 {
                     this.props.utilities && this.props.utilities.FAQ.success ?
-                        <View style={{marginHorizontal: 20}}>
-                            <HTML
-                                html={this.props.utilities.FAQ.result.faqHtmlContent}
-                                imagesMaxWidth={Dimensions.get('window').width}
-                            />
-                        </View>
-                        : null
-                }
+                        <WebView
+                            originWhitelist={['*']}
+                            source={{ html: this.props.utilities.FAQ.result.faqHtmlContent }}
+                        /> : <View />
 
-            </ScrollView>
+                }
+            </View>
         );
     }
 
