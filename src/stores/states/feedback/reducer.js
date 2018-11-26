92377c36b6122f42cb730b3d40f7e82574f8b454
ThinhNh <thinhnh@dinovative.com>
2018-11-26 18:26:17 +0700

    -fix

--- src/stores/states/feedback/reducer.js
+++ src/stores/states/feedback/reducer.js
@@ -1,173 +1,163 @@
 import Types from './';
 import createReducer from '../';
 
-const INIT_STATE = ({
-    listFeedBack: { items: [], pageCount: 0, success: false },
-    listCategory: {},
-    typeFeedback: {},
-    createFeedback: {}
-});
+const INIT_STATE = {
+  listFeedBack: { items: [], pageCount: 0, success: false },
+  listCategory: {},
+  typeFeedback: {},
+  createFeedback: {}
+};
 
 export default createReducer(INIT_STATE, {
+  [Types.GET_LIST_FEEDBACK]: (state, action) => {
+    try {
+      return {
+        ...state
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    [Types.GET_LIST_FEEDBACK]: (state, action) => {
-        try {
-            return {
-                ...state
-            };
-        } catch (error) {
-            console.log(error)
+  [Types.GET_LIST_FEEDBACK_SUCCESS]: (state, action) => {
+    try {
+      let tempState;
+      tempState = Object.assign(
+        {},
+        { ...state },
+        {
+          listFeedBack: {
+            items: action.response.result.items,
+            success: action.response.success,
+            pageCount: action.response.result.pageCount
+          }
         }
+      );
+      return tempState;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    },
+  [Types.GET_LIST_FEEDBACK_FAIL]: (state, action) => {
+    try {
+      return {
+        ...state,
+        listFeedBack: {}
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    [Types.GET_LIST_FEEDBACK_SUCCESS]: (state, action) => {
-        try {
-            let tempState;
-            tempState = Object.assign(
-                {},
-                { ...state },
-                {
-                    listFeedBack: {
-                        items: action.response.result.items,
-                        success: action.response.success,
-                        pageCount: action.response.result.pageCount
-                    },
-                },
-            );
-            return tempState;
-        } catch (error) {
-            console.log(error)
+  [Types.GET_LIST_CATEGORY]: (state, action) => {
+    try {
+      return {
+        ...state
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  [Types.GET_LIST_CATEGORY_SUCCESS]: (state, action) => {
+    try {
+      let tempState;
+      tempState = Object.assign(
+        {},
+        { ...state },
+        {
+          listCategory: action.response
         }
-    },
+      );
+      return tempState;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    [Types.GET_LIST_FEEDBACK_FAIL]: (state, action) => {
-        try {
-            return {
-                ...state,
-                listFeedBack: {},
-            };
-        } catch (error) {
-            console.log(error)
+  [Types.GET_LIST_CATEGORY_FAIL]: (state, action) => {
+    try {
+      return {
+        ...state,
+        listCategory: {}
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  [Types.GET_LIST_TYPE_FEEDBACK]: (state, action) => {
+    try {
+      return {
+        ...state
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  [Types.GET_LIST_TYPE_FEEDBACK_SUCCESS]: (state, action) => {
+    try {
+      let tempState;
+      tempState = Object.assign(
+        {},
+        { ...state },
+        {
+          typeFeedback: action.response
         }
-    },
+      );
+      return tempState;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    [Types.GET_LIST_CATEGORY]: (state, action) => {
-        try {
-            return {
-                ...state
-            };
-        } catch (error) {
-            console.log(error)
+  [Types.GET_LIST_TYPE_FEEDBACK_FAIL]: (state, action) => {
+    try {
+      return {
+        ...state,
+        typeFeedback: {}
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  [Types.CREATE_FEEDBACK]: (state, action) => {
+    try {
+      return {
+        ...state
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  [Types.CREATE_FEEDBACK_SUCCESS]: (state, action) => {
+    try {
+      let tempState;
+      tempState = Object.assign(
+        {},
+        { ...state },
+        {
+          createFeedback: action.response
         }
+      );
+      return tempState;
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-    },
-
-    [Types.GET_LIST_CATEGORY_SUCCESS]: (state, action) => {
-        try {
-            let tempState;
-            tempState = Object.assign(
-                {},
-                { ...state },
-                {
-                    listCategory: action.response
-                },
-            );
-            return tempState;
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-    [Types.GET_LIST_CATEGORY_FAIL]: (state, action) => {
-        try {
-            return {
-                ...state,
-                listCategory: {},
-            };
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-
-    [Types.GET_LIST_TYPE_FEEDBACK]: (state, action) => {
-        try {
-            return {
-                ...state
-            };
-        } catch (error) {
-            console.log(error)
-        }
-
-    },
-
-    [Types.GET_LIST_TYPE_FEEDBACK_SUCCESS]: (state, action) => {
-        try {
-            let tempState;
-            tempState = Object.assign(
-                {},
-                { ...state },
-                {
-                    typeFeedback: action.response
-                },
-            );
-            return tempState;
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-    [Types.GET_LIST_TYPE_FEEDBACK_FAIL]: (state, action) => {
-        try {
-            return {
-                ...state,
-                typeFeedback: {},
-            };
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-
-    [Types.CREATE_FEEDBACK]: (state, action) => {
-        try {
-            return {
-                ...state
-            };
-        } catch (error) {
-            console.log(error)
-        }
-
-    },
-
-    [Types.CREATE_FEEDBACK_SUCCESS]: (state, action) => {
-        try {
-            let tempState;
-            tempState = Object.assign(
-                {},
-                { ...state },
-                {
-                    createFeedback: action.response
-                },
-            );
-            return tempState;
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-    [Types.CREATE_FEEDBACK_FAIL]: (state, action) => {
-        try {
-            return {
-                ...state,
-                createFeedback: {},
-            };
-        } catch (error) {
-            console.log(error)
-        }
-    },
-
-
-
+  [Types.CREATE_FEEDBACK_FAIL]: (state, action) => {
+    try {
+      return {
+        ...state,
+        createFeedback: {}
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
