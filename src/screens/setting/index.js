6ff18a09586ab2efccafe2edfa0838442ecce98e
Nhieu <nhjuhoang@gmail.com>
2018-12-30 22:53:09 +0700

    fix header setting

--- src/screens/setting/index.js
+++ src/screens/setting/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Animated } from 'react-native';
+import { Animated, Platform } from 'react-native';
 import Connect from '@stores';
 import layout from './layout';
 
 import _ from 'lodash';
+
+const HEADER_MAX_HEIGHT = 60;
 
 class Setting extends layout {
   constructor(props) {
@@ -12,7 +14,7 @@ class Setting extends layout {
     let setting = this.props.app.getSetting;
     this.state = {
       isModalSelectUnit: false,
-      scrollY: new Animated.Value(0),
+      scrollY: new Animated.Value(Platform.OS === 'ios' ? -HEADER_MAX_HEIGHT : 0),
       selectedItem: this.props.app.languegeLocal,
       dataSetting: {
         emailFee: setting[9].isSubscribed,
